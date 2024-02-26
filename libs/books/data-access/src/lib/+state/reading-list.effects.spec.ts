9815cd378f06df2bcb6068ca3d391060c018b655
Jesin Jaice <j99615d@gmail.com>
2024-02-27 00:33:47 +0530

    implemented finished functionality

--- libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
@@ -1,17 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, Subject } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { HttpTestingController } from '@angular/common/http/testing';
 
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { SharedTestingModule, createReadingListItem  } from '@tmo/shared/testing';
 import { ReadingListEffects } from './reading-list.effects';
 import * as ReadingListActions from './reading-list.actions';
+import { takeUntil } from 'rxjs/operators';
 
 describe('ToReadEffects', () => {
   let actions: ReplaySubject<any>;
   let effects: ReadingListEffects;
   let httpMock: HttpTestingController;
+  let unsubscribe$: Subject<void>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,14 +27,21 @@ describe('ToReadEffects', () => {
 
     effects = TestBed.inject(ReadingListEffects);
     httpMock = TestBed.inject(HttpTestingController);
+    unsubscribe$ = new Subject<void>();
   });
+
+  afterEach (()=>{
+    unsubscribe$.next();
+    unsubscribe$.complete();
+  })
 
   describe('loadReadingList$', () => {
     it('should work', done => {
       actions = new ReplaySubject();
       actions.next(ReadingListActions.init());
 
-      effects.loadReadingList$.subscribe(action => {
+      effects.loadReadingList$.pipe(takeUntil(unsubscribe$))
+      .subscribe(action => {
         expect(action).toEqual(
           ReadingListActions.loadReadingListSuccess({ list: [] })
         );
@@ -40,6 +49,25 @@ describe('ToReadEffects', () => {
       });
 
       httpMock.expectOne('/api/reading-list').flush([]);
+    });
+  });
+
+  describe('markBookAsFinished$', () => {
+    it('should mark the book as finished when user clicks on button ', done => {
+      actions = new ReplaySubject();
+      const Item = createReadingListItem('A');
+      const finishedDate = new Date().toISOString();
+      actions.next(ReadingListActions.markBookAsFinished({ item: Item, finishedDate: finishedDate }));
+
+      effects.markBookAsFinished$.pipe(takeUntil(unsubscribe$))
+      .subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.markBookAsFinished({ item: Item, finishedDate: finishedDate })
+          );
+        done();
+      });
+
+      httpMock.expectOne('/api/reading-list/A/finished').flush({ item: Item, finishedDate: finishedDate, type: '[Reading List API] Mark book as finished' })      
     });
   });
 });
