97c350a199b4fb2876843348d59d41e6f297f827
Jesin Jaice <j99615d@gmail.com>
2024-02-26 17:56:42 +0530

    added undo add and remove snackbar

--- libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
@@ -1,23 +1,41 @@
-import { TestBed } from '@angular/core/testing';
-import { ReplaySubject } from 'rxjs';
+import { TestBed, fakeAsync } from '@angular/core/testing';
+import { ReplaySubject, Subject } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore,MockStore } from '@ngrx/store/testing';
 import { HttpTestingController } from '@angular/common/http/testing';
 
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { SharedTestingModule,createBook, createReadingListItem } from '@tmo/shared/testing';
 import { ReadingListEffects } from './reading-list.effects';
 import * as ReadingListActions from './reading-list.actions';
+
+import { MatSnackBar, MatSnackBarModule, MatSnackBarRef } from '@angular/material/snack-bar';
+import { Book, ReadingListItem } from '@tmo/shared/models';
+import { takeUntil } from 'rxjs/operators';
 
 describe('ToReadEffects', () => {
   let actions: ReplaySubject<any>;
   let effects: ReadingListEffects;
   let httpMock: HttpTestingController;
+  let item: ReadingListItem;
+  let book: Book;
+  let snackbar: MatSnackBar;
+  let store : MockStore;
+  let unsubscribe$: Subject<void>;
+
+  const bookItem = createReadingListItem('A');
+  const itemBook = createBook('B');
+
+  beforeAll(() => {
+    item = createReadingListItem('A');      
+    book = createBook('A');
+  })
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SharedTestingModule],
+      imports: [SharedTestingModule, MatSnackBarModule],
       providers: [
         ReadingListEffects,
+        { provide: MatSnackBar, useVale: {open: (param1, param2) => { return; }}},
         provideMockActions(() => actions),
         provideMockStore()
       ]
@@ -25,14 +43,23 @@ describe('ToReadEffects', () => {
 
     effects = TestBed.inject(ReadingListEffects);
     httpMock = TestBed.inject(HttpTestingController);
+    actions = new ReplaySubject();
+    store = TestBed.inject(MockStore);
+    snackbar = TestBed.inject(MatSnackBar);
+    unsubscribe$ = new Subject<void>();
   });
 
+  afterEach (()=>{
+    unsubscribe$.next();
+    unsubscribe$.complete();
+  })
+
   describe('loadReadingList$', () => {
-    it('should work', done => {
-      actions = new ReplaySubject();
+    it('should fetch reading list', done => {
       actions.next(ReadingListActions.init());
 
-      effects.loadReadingList$.subscribe(action => {
+      effects.loadReadingList$.pipe(takeUntil(unsubscribe$))
+      .subscribe(action => {
         expect(action).toEqual(
           ReadingListActions.loadReadingListSuccess({ list: [] })
         );
@@ -42,4 +69,89 @@ describe('ToReadEffects', () => {
       httpMock.expectOne('/api/reading-list').flush([]);
     });
   });
+
+  describe('addBook$', () => {
+    it('should add a book to the reading list successfully', fakeAsync(() => {
+      actions.next(ReadingListActions.addToReadingList({ book, add: false  }));
+
+      effects.addBook$.pipe(takeUntil(unsubscribe$))
+      .subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.confirmedAddToReadingList({ book, add: false })
+        );
+      })
+
+      httpMock.expectOne('/api/reading-list').flush({});
+    }));
+
+    it('should undo the added book when API returns error', fakeAsync(() => {
+      actions.next(ReadingListActions.addToReadingList({ book, add: false}));
+
+      effects.addBook$.pipe(takeUntil(unsubscribe$))
+      .subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.failedAddToReadingList({ book })
+        );
+      });
+
+      httpMock.expectOne('/api/reading-list').flush({}, { status: 500, statusText: 'server error' });
+    }));
+
+    it('show snackbar on add book', async() => {
+      actions = new ReplaySubject();
+      actions.next(ReadingListActions.confirmedAddToReadingList({ book, add:false }));
+
+      effects.addBook$.pipe(takeUntil(unsubscribe$))
+        .subscribe(() => {
+        snackbar.open(`Added ${book.title} to reading list`,'Undo', { duration: 2000}).onAction().subscribe((action)=>{
+          expect(action).toEqual(
+            ReadingListActions.removeFromReadingList({ item:bookItem, remove:true }))
+        })
+      });
+    });
+  }); 
+
+  describe('removeBook$', () => {
+    it('should remove book successfully from reading list', done => {
+      actions.next(ReadingListActions.removeFromReadingList({ item, remove: false }));
+
+      effects.removeBook$.pipe(takeUntil(unsubscribe$))
+      .subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.confirmedRemoveFromReadingList({ item, remove: false  })
+        );
+        done();
+      });
+
+      httpMock.expectOne(`/api/reading-list/${item.bookId}`).flush({});
+    });
+
+    it('should undo removed book when API returns error', fakeAsync(() => {
+      actions.next(ReadingListActions.removeFromReadingList({ item, remove: false  }));
+
+      effects.removeBook$.pipe(takeUntil(unsubscribe$))
+      .subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.failedRemoveFromReadingList({ item })
+        );
+      });
+
+      httpMock.expectOne(`/api/reading-list/${item.bookId}`).flush({}, { status: 500, statusText: 'server error' });
+    }));
+
+    it('show snackbar on remove book', async() => {
+      actions = new ReplaySubject();
+      actions.next(ReadingListActions.confirmedRemoveFromReadingList({ item, remove:false }));
+
+      effects.removeBook$.pipe(takeUntil(unsubscribe$))
+        .subscribe(() => {
+        snackbar.open(`Removed ${item.title} to reading list`,'Undo', { duration: 2000}).onAction().subscribe((action)=>{
+          expect(action).toEqual(
+            ReadingListActions.addToReadingList({ book:itemBook, add:true })
+          );
+        })
+      });
+    });
+  });
+
 });
--- libs/books/data-access/src/lib/+state/reading-list.effects.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType, OnInitEffects } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { catchError, concatMap, exhaustMap, map } from 'rxjs/operators';
-import { ReadingListItem } from '@tmo/shared/models';
+import { catchError, concatMap, exhaustMap, map, tap, switchMap } from 'rxjs/operators';
+import { ReadingListItem, Book } from '@tmo/shared/models';
 import * as ReadingListActions from './reading-list.actions';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class ReadingListEffects implements OnInitEffects {
@@ -27,9 +29,9 @@ export class ReadingListEffects implements OnInitEffects {
   addBook$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ReadingListActions.addToReadingList),
-      concatMap(({ book }) =>
+      concatMap(({ book, add }) =>
         this.http.post('/api/reading-list', book).pipe(
-          map(() => ReadingListActions.confirmedAddToReadingList({ book })),
+          map(() => ReadingListActions.confirmedAddToReadingList({ book, add })),
           catchError(() =>
             of(ReadingListActions.failedAddToReadingList({ book }))
           )
@@ -41,10 +43,10 @@ export class ReadingListEffects implements OnInitEffects {
   removeBook$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ReadingListActions.removeFromReadingList),
-      concatMap(({ item }) =>
+      concatMap(({ item, remove }) =>
         this.http.delete(`/api/reading-list/${item.bookId}`).pipe(
           map(() =>
-            ReadingListActions.confirmedRemoveFromReadingList({ item })
+            ReadingListActions.confirmedRemoveFromReadingList({ item, remove })
           ),
           catchError(() =>
             of(ReadingListActions.failedRemoveFromReadingList({ item }))
@@ -54,9 +56,51 @@ export class ReadingListEffects implements OnInitEffects {
     )
   );
 
+  showSnackBarOnAdd$=createEffect(() => 
+    this.actions$.pipe(
+      ofType(ReadingListActions.confirmedAddToReadingList),
+      concatMap((action) => {
+        const typeItem:ReadingListItem = {
+          ...action.book,
+          bookId: action.book.id
+        }
+        if(action.add === false){
+          return this.snackBar.open(
+            `Added ${action.book.title} to reading list`,'Undo', { duration: 2000}
+            ).onAction().pipe(map(() =>
+            ReadingListActions.removeFromReadingList({ item:typeItem, remove:true })
+            ))
+        }
+        else
+        return [];
+      })
+    )
+  );
+
+  showSnackBarOnRemove$=createEffect(() => 
+    this.actions$.pipe(
+      ofType(ReadingListActions.confirmedRemoveFromReadingList),
+      concatMap((action) => {
+        const typeBook:Book = {
+        id: action.item.bookId,
+        ...action.item
+      }
+      if(action.remove === false){
+        return this.snackBar.open(
+          `Removed ${action.item.title} to reading list`,'Undo', { duration: 2000}
+          ).onAction().pipe(map(() =>
+          ReadingListActions.addToReadingList({ book:typeBook, add:true })
+          ))
+      } 
+      else
+        return [];
+      })
+    )
+  )
+
   ngrxOnInitEffects() {
     return ReadingListActions.init();
   }
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(private actions$: Actions, private http: HttpClient, private snackBar: MatSnackBar, private readonly store: Store) { }
 }
